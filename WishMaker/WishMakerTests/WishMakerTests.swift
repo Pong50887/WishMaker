ab4fa670671987672b94005495a95626e6fda921
Pong50887 <pichayoot.pong@gmail.com>
2025-05-08 22:55:01 +0700

    Initial Xcode

---
+++ WishMaker/WishMakerTests/WishMakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WishMakerTests.swift
+//  WishMakerTests
+//
+//  Created by Punpun on 8/5/25.
+//
+
+import Testing
+@testable import WishMaker
+
+struct WishMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
